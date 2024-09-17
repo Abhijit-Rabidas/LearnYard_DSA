3bfa192489bc998218f16ee580568b3e4f555235
Abhijit Rabidas <aaacs.hackers101@gmail.com>
2024-09-17 21:38:23 +0530

    Update Q2.cpp

---
+++ WEEK-3/2_Home-Work/Q2.cpp
@@ -0,0 +1,33 @@
+/*
+Input a number n and print the below pattern.
+let, n = 5
+
+        5
+        44
+        333
+        2222
+        11111
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout << n - i + 1;
+        }
+        cout << endl;
+        
+    }
+
+    return 0;
+}
