cf1b3b688da54e8781a61505baf530d2a9968b44
Abhijit Rabidas <aaacs.hackers101@gmail.com>
2024-09-17 20:40:50 +0530

    Update Q3.cpp

--- WEEK-2/1_Class-Questions/Q3.cpp
+++ WEEK-2/1_Class-Questions/Q3.cpp
@@ -63,30 +63,8 @@ int main()
 //     return 0;
 // }
 
+
 // // METHOD_3:
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// int main()
-// {
-//     int year;
-
-//     cout << "Enter year: ";
-//     cin >> year;
-
-//     if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
-//     {
-//         cout << year << " is a Leap Year";
-//     }
-//     else
-//     {
-//         cout << year << " is not a Leap Year";
-//     }
-
-//     return 0;
-// }
-
-// // METHOD_4:
 // #include <bits/stdc++.h>
 // using namespace std;
 
