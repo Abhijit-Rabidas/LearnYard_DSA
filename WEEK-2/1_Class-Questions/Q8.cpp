30ba098168083508829f0977a50ec0eb4c5aa0ee
Abhijit Rabidas <aaacs.hackers101@gmail.com>
2024-09-23 13:51:33 +0530

    Update Q8.cpp

--- WEEK-2/1_Class-Questions/Q8.cpp
+++ WEEK-2/1_Class-Questions/Q8.cpp
@@ -128,3 +128,40 @@ int main()
 
 //     return 0;
 // }
+
+/*
+// METHOD_4:
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    
+    if (n<1)
+    {
+        cout << n << " is not prime";
+        return 0;
+    }
+    int count = 0;
+    for(int i = 1; i<=n; i++)
+    {
+        if(n%i == 0)
+        {
+            count++;
+        }
+    }
+    
+    if(count > 2)
+    {
+        cout << n << " not is Prime";
+    }
+    else
+    {
+        cout << n << " is Prime";
+    }
+
+    return 0;
+}
+
+*/
