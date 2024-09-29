343aaf4a052f67ef50f2d31809d0002de027df09
Abhijit Rabidas <aaacs.hackers101@gmail.com>
2024-09-29 10:35:57 +0530

    Update Q14.cpp

--- WEEK-2/1_Class-Questions/Q14.cpp
+++ WEEK-2/1_Class-Questions/Q14.cpp
@@ -23,3 +23,40 @@ int main()
 
     return 0;
 }
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int sumOfDigits(int n)
+{
+    if(n == 0)
+    {
+        cout << 0;
+        return 0;
+    }
+    else
+    {
+        int sum = 0;
+        while(n>0)
+        {
+            sum += n % 10;
+            n = n / 10;
+        }
+        return sum;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    for(int i=1; i<=n; i++)
+    {
+        cout << sumOfDigits(i) <<" ";
+    }
+    
+    return 0;
+}
+*/
