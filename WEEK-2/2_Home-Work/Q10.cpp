d4116c4c670c40a168307a1d9dd0a2ce7ea66822
Abhijit Rabidas <aaacs.hackers101@gmail.com>
2024-09-30 11:40:52 +0530

    Update Q10.cpp

--- WEEK-2/2_Home-Work/Q10.cpp
+++ WEEK-2/2_Home-Work/Q10.cpp
@@ -1,5 +1,85 @@
 //  Print all prime numbers from 1 to N.
 
+//METHOD_1:
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (isPrime(i) == true)
+        {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
+
+/*
+//METHOD_2:
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+  int count = 0;
+  for(int i = 1; i<=n; i++)
+  {
+    if(n%i == 0)
+    {
+      count++;
+    }
+  }
+  
+  if(n==1 ||count>2)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+int main() 
+{
+    int n;
+    cin >> n;
+    
+    for(int i=1;i<n;i++)
+    {
+      if(isPrime(i))
+      {
+        cout << i << " ";
+      }
+    }
+    return 0;
+}
+*/
+
+/*
+// METHOD_3:
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -36,3 +116,4 @@ int main()
 
     return 0;
 }
+*/
