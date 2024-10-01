4939cd14666b6dc4b1c01522cde9b103656b2773
Abhijit Rabidas <aaacs.hackers101@gmail.com>
2024-10-01 13:02:36 +0530

    Update Q3.cpp

--- WEEK-3/1_Class-Questions/Q3.cpp
+++ WEEK-3/1_Class-Questions/Q3.cpp
@@ -50,3 +50,42 @@ int main()
 
     return 0;
 }
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin >> n;
+    
+    int row = n*2-1;
+    int col = 1;
+    int space = n-1;
+    
+    for(int i = 1; i<=row; i++)
+    {
+      for(int k = space; k>=1; k--)
+      {
+        cout << " ";
+      }
+      for(int j = 1; j<= col; j++)
+      {
+        cout << "*";
+      }
+      cout << endl;
+      if(i<n)
+      {
+        space--;
+        col +=2;
+      }
+      else
+      {
+        space++;
+        col-=2;
+      }
+    }
+    return 0;
+}
+*/
